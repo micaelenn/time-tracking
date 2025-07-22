6d3692f84548464866158daee65d69e109dadb2d
micaelenn <mirandamicaelen@gmail.com>
2025-07-22 18:55:08 -0300

    feat(api): add condition to manage the database connection

--- backend/config/application.ts
+++ backend/config/application.ts
@@ -1,4 +1,7 @@
-export class App {}
+export class App {
+  static connectDatabase: string | undefined = process.env.CONNECT_DATABASE;
+  static port: string| undefined = process.env.PORT
+}
 
 export const setEndpoint = (endpoint: string) => {
   return `/api/v1/${endpoint}`
--- backend/server.ts
+++ backend/server.ts
@@ -1,13 +1,21 @@
-import { app } from "@/app/routes";
+// external modules
+import colors from 'colors'
+
+// internal modules
 import { sequelize } from '@/config/database'
+import { app } from "@/app/routes";
+import { App } from "@/config/application"
 
 // init sequelize
-(async () => {
-  await sequelize.sync({ alter: true });
-})(); 
+if ( App.connectDatabase === "true") {
+  (async () => {
+    await sequelize.sync({ alter: true });
+  })(); 
+} 
 
-const PORT = 3000
+const PORT = App.port || 8080
+const DB_CONENCTION = App.connectDatabase
 
 app.listen(PORT , () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server Running On ${colors.cyan(`http://localhost:${PORT}`)} | Allow Database Connection: ${colors.cyan(`${DB_CONENCTION}`)}`);
 });
